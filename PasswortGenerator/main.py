58a401addfbdd625d509d0e94fcefa347833bc3d
A. Draca <andrija.dracaa@gmail.com>
2022-04-30 18:27:02 +0200

    Passwort Generator Verzeichnis erstellt und die erste kleine GUI erstellt.

---
+++ PasswortGenerator/main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+# Main Gui
+window = tk.Tk()
+window.geometry("800x450")
+window.title("Passwort Generator")
+window.resizable(0,0)
+window.configure(bg="#424242")
+
+window.mainloop()
