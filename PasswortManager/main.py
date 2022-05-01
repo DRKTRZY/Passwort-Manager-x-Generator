14d37b31f3904b3436d90d36993a4de033b098ac
Victor Gashi <victorgashimartinez@gmail.com>
2022-05-01 15:13:06 +0200

    Ich habe die Gui des Loginscreen für den Passwort Manager fertig gestellt.

--- PasswortManager/main.py
+++ PasswortManager/main.py
@@ -1,3 +1,24 @@
 import sqlite3, hashlib
 from tkinter import *
 
+window = Tk()
+
+window.title("Password Manager")
+window.geometry("500x400")
+
+def loginscreen():
+    window.geometry("350x200")
+
+lbl = Label(window,pady=10,text="Geben Sie das Master Passwort ein")
+lbl.config(anchor=CENTER)
+lbl.pack()
+
+entry = Entry(window,width=20)
+entry.pack()
+
+btn = Button(window,width=10,text="Enter")
+btn.pack(pady=10)
+
+loginscreen()
+window.mainloop()
+
