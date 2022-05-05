8be320c1e49b6101ba2ffe8becb1dd587a36b356
A. Draca <andrija.dracaa@gmail.com>
2022-05-05 22:57:34 +0200

    Die GUI hat jetzt noch erweiterte Funktionen. Mit dem korrekten Passwort öffnet sich ein neues Fenster.

--- PasswortManager/main.py
+++ PasswortManager/main.py
@@ -1,7 +1,7 @@
 import sqlite3, hashlib
-from tkinter import *
+import tkinter as tk
 
-window = Tk()
+window = tk.Tk()
 
 window.title("Password Manager")
 window.geometry("500x400")
@@ -9,16 +9,33 @@ window.geometry("500x400")
 def loginscreen():
     window.geometry("350x200")
 
-lbl = Label(window,pady=10,text="Geben Sie das Master Passwort ein")
-lbl.config(anchor=CENTER)
+lbl = tk.Label(window,pady=10,text="Geben Sie das Master Passwort ein")
+lbl.config(anchor=tk.CENTER)
 lbl.pack()
 
-entry = Entry(window,width=20)
-entry.pack()
+error_lbl = tk.Label(window)
+error_lbl.pack()
 
-btn = Button(window,width=10,text="Enter")
+entry = tk.Entry(window,width=20,show="*")
+entry.pack()
+entry.focus()
+
+def check_password():
+    password = "hay"
+    if password == entry.get():
+        password_vault()
+    else:
+        entry.delete(0, 'end')
+        error_lbl.config(text="Falsches Passwort")
+btn = tk.Button(window,width=10,text="Enter",command=check_password)
 btn.pack(pady=10)
 
+def password_vault():
+    for widget in window.winfo_children():
+        widget.destroy()
+    window.geometry("700x300")
+
+    correct_lbl = tk.Label(window,text="Passwort ist korrekt",anchor=tk.CENTER)
+    correct_lbl.pack()
 loginscreen()
 window.mainloop()
-
