4f015aefe4a3b6c78f49d698aeea421bb528a3a5
A. Draca <andrija.dracaa@gmail.com>
2022-05-11 19:57:32 +0200

    Ich habe ein First Screen erstellt. Als nächstes kommt SQL Datenbanken

--- PasswortGenerator/alt_strings.py
+++
@@ -1,4 +0,0 @@
-digit = ("123456789012345678901234567890123456789012345678901234567890")
-upper = ("QWERTZUIOPASDFGHJKLYXCVBNMQWERTZUIOPASDFGHJKLYXCVBNMEDXSFTGU")
-lower = ("qwertzuiopasdfghjklyxcvbnmqwertzuiopasdfghjklyxcvbnmqasfnawsz")
-symbols = ('''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~!$%&'(*+,-./:;<=>@[\]^_`{|}~''')
--- PasswortManager/main.py
+++ PasswortManager/main.py
@@ -6,29 +6,61 @@ window = tk.Tk()
 window.title("Password Manager")
 window.geometry("500x400")
 
-def loginscreen():
-    window.geometry("350x200")
+def first_screen():
+    window.geometry("350x300")
 
-lbl = tk.Label(window,pady=10,text="Geben Sie das Master Passwort ein")
-lbl.config(anchor=tk.CENTER)
-lbl.pack()
+    mstr_pswrd = tk.Label(window, pady=10, text="Erstelle ein Master Passwort")
+    mstr_pswrd.config(anchor=tk.CENTER)
+    mstr_pswrd.pack()
 
-error_lbl = tk.Label(window)
-error_lbl.pack()
+    re_enter_lbl = tk.Label(window, pady=10, text="Geben sie das Passwort erneut ein")
+    re_enter_lbl.pack()
 
-entry = tk.Entry(window,width=20,show="*")
-entry.pack()
-entry.focus()
+    error_lbl = tk.Label(window)
+    error_lbl.pack()
 
-def check_password():
-    password = "hay"
-    if password == entry.get():
-        password_vault()
-    else:
-        entry.delete(0, 'end')
-        error_lbl.config(text="Falsches Passwort")
-btn = tk.Button(window,width=10,text="Enter",command=check_password)
-btn.pack(pady=10)
+    entry = tk.Entry(window, width=20, show="*")
+    entry.pack()
+    entry.focus()
+
+    re_enter = tk.Entry(window,width=20)
+    re_enter.pack()
+    re_enter.focus()
+
+    def save_password():
+        if entry.get() == re_enter.get():
+            error_lbl.config(text="")
+            pass
+        else:
+            error_lbl.config(text="Passwörter stimmen nicht mit einander ein")
+
+    btn = tk.Button(window,width=10,text="Save",command=save_password)
+    btn.pack(pady=10)
+
+def login_screen():
+    window.geometry("350x300")
+
+    lbl = tk.Label(window,pady=10,text="Geben sie das Master password ein")
+    lbl.config(anchor=tk.CENTER)
+    lbl.pack()
+
+    error_lbl = tk.Label(window)
+    error_lbl.pack()
+
+    entry = tk.Entry(window,width=20,show="*")
+    entry.pack()
+    entry.focus()
+
+    def check_password():
+        password = "hey"
+        if password == entry.get():
+            password_vault()
+        else:
+            entry.delete(0, 'end')
+            error_lbl.config(text="Falsches Passwort")
+
+    btn = tk.Button(window,width=10,text="Enter",command=check_password)
+    btn.pack(pady=10)
 
 def password_vault():
     for widget in window.winfo_children():
@@ -37,5 +69,5 @@ def password_vault():
 
     correct_lbl = tk.Label(window,text="Passwort ist korrekt",anchor=tk.CENTER)
     correct_lbl.pack()
-loginscreen()
+first_screen()
 window.mainloop()
